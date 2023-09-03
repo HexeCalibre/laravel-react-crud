5a368aada4fcdf2407aa7e06fc1442749c59a88f
akosialbean <alvincastor30@gmail.com>
2023-09-03 21:07:08 +0800

    Adding initial files for frontend

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function view(){
+        $data = User::all();
+        return response()->json($data);
+    }
+}
