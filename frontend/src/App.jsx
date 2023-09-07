a2fabf40a7ee8726bc9a097f437218cc43fa8f01
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2023-09-07 09:12:33 +0800

    Add Create Description Component (WIP)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import PageLinks from "./parts/PageLinks.jsx";
 import CreateUser from "./components/CreateUser.jsx";
 import UseEffectComponent from "./components/UseEffectComponent.jsx";
 import UserList from "./pages/UserList.jsx";
+import Create from "./pages/Create.jsx";
+import Login from "./components/Login.jsx";
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
             <Route path="createnewuser" element={<CreateUser />} />
             <Route path="useeffect" element={<UseEffectComponent />} />
             <Route path="userlist" element={<UserList />} />
+            <Route path="create" element={<Create />} />
+            <Route path="login" element={<Login />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- frontend/src/parts/PageLinks.jsx
+++ frontend/src/parts/PageLinks.jsx
@@ -5,12 +5,14 @@ const PageLinks = () => {
     <>
         <ul>
             <li><a href="/">View</a></li>
+            <li><a href="/login">Already have an account?</a></li>
             <li><a href="/createnewuser">Create New User</a></li>
             <li><a href="/clickcount">Click Count</a></li>
             <li><a href="/events">Events</a></li>
             <li><a href="/welcome">Welcome</a></li>
             <li><a href="/useeffect">Use Effect</a></li>
             <li><a href="/userlist">User List</a></li>
+            <li><a href="/create">Create Description</a></li>
         </ul>
     </>
   )
