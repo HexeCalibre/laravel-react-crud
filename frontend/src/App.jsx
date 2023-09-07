991175e7fe9cea7d34156ef3114b22d80ca3c1f7
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2023-09-07 18:34:50 +0800

    accept incoming code changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,14 +7,9 @@ import PageLinks from "./parts/PageLinks.jsx";
 import CreateUser from "./components/CreateUser.jsx";
 import UseEffectComponent from "./components/UseEffectComponent.jsx";
 import UserList from "./pages/UserList.jsx";
-<<<<<<< HEAD
-import Create from "./pages/Create.jsx";
-import Login from "./components/Login.jsx";
-=======
 import View from "./pages/View.jsx";
 import Create from "./pages/Create.jsx";
 import Description from "./pages/Description.jsx";
->>>>>>> 7c2940b7eefce6667fa69805a5bd6cc941fbf27b
 
 function App() {
   return (
@@ -30,14 +25,9 @@ function App() {
             <Route path="createnewuser" element={<CreateUser />} />
             <Route path="useeffect" element={<UseEffectComponent />} />
             <Route path="userlist" element={<UserList />} />
-<<<<<<< HEAD
-            <Route path="create" element={<Create />} />
-            <Route path="login" element={<Login />} />
-=======
             <Route path="view" element={<View />} />
             <Route path="create" element={<Create />} />
             <Route path="description/:id" element={<Description />} />
->>>>>>> 7c2940b7eefce6667fa69805a5bd6cc941fbf27b
           </Route>
         </Routes>
       </BrowserRouter>
