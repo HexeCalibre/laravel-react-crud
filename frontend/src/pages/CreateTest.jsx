e5f5dd1bacd9368d3795841a781292863f2a62f9
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2023-09-08 09:44:09 +0800

    CreateTest.jsx Updated

--- frontend/src/pages/CreateTest.jsx
+++ frontend/src/pages/CreateTest.jsx
@@ -7,8 +7,45 @@ const CreateTest = () => {
   // Handle value for Description textbox
   // Class setter
   const [description, setDescription] = useState("");
-  const descriptionHandleChange = (e) => {
-    setDescription(e.target.value);
+  // This section has been encapsulated directly to the <input> element tag
+  //   const descriptionHandleChange = (e) => {
+  //     setDescription(e.target.value);
+  //   };
+
+  // URL function
+  const navigate = useNavigate();
+
+  // Form Submission
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const url = `${import.meta.env.VITE_API_URL}/create`;
+
+    //HTTP Request Method
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Object literal
+      // Use the 'description' variable from state
+      body: JSON.stringify({
+        description: description,
+      }),
+    };
+
+    // API request & response
+    fetch(url, requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        navigate("/view");
+      })
+      .catch((err) => console.error(err));
   };
 
   // Button Add Description
@@ -20,22 +57,24 @@ const CreateTest = () => {
     setMessage(`${description}`);
   };
 
+  // GUI Render
   return (
     <>
       <h2>Create Description</h2>
-      {/* <form> */}
-      <input
-        type="text"
-        name="description"
-        placeholder="type description here"
-        onChange={descriptionHandleChange}
-        required
-      />
-      <button type="submit" onClick={handleClick}>
-        Save
-      </button>
-      <p>New Description Added: {message}</p>
-      {/* </form> */}
+      <form className="add-form" onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="description"
+          placeholder="Add Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        />
+        <button type="submit" value="Save Task">
+          Save
+        </button>
+        <p>New Description Added: {message}</p>
+      </form>
     </>
   );
 };
